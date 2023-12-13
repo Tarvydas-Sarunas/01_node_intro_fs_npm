a93a860c0876c2bef50f266bba66355806c6f2b7
SarunasT <sarunas.tarvydas@hotmail.com>
2023-12-13 08:31:43 +0100

    add

--- intro.js
+++ intro.js
@@ -5,3 +5,12 @@ function sum(sk1, sk2) {
   console.log(sk1 + sk2);
 }
 sum(15, 45);
+
+function getAvg(arr) {
+  let sum = 0;
+  arr.forEach((element) => (sum += element));
+  return sum / arr.length;
+}
+
+const avg = getAvg([1, 2, 3]);
+console.log(avg);
